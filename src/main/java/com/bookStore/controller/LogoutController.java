0fd2d5841a0d0b4f99564dd9ba3413104bee8b38
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-24 23:03:21 +0300

    feat: Добавил логику "Выход из профиля"

---
+++ src/main/java/com/bookStore/controller/LogoutController.java
@@ -0,0 +1,21 @@
+package com.bookStore.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout() {
+        // Используйте Spring Security для выхода из профиля
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            SecurityContextHolder.getContext().setAuthentication(null);
+        }
+        // Перенаправьте на главную страницу или куда-либо еще после выхода из профиля
+        return "redirect:/auth/action-selection-form";
+    }
+}
