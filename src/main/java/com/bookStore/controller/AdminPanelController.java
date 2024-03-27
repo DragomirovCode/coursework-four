2fa3de710350bf39b2e4e6d89d8242425107c26b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-27 12:25:49 +0300

    feat: Добавил метод "вернуть всех людей"

--- src/main/java/com/bookStore/controller/AdminPanelController.java
+++ src/main/java/com/bookStore/controller/AdminPanelController.java
@@ -1,17 +1,24 @@
 package com.bookStore.controller;
 
 import com.bookStore.entity.Book;
+import com.bookStore.entity.Person;
 import com.bookStore.service.BookService;
+import com.bookStore.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class AdminPanelController {
     @Autowired
     private BookService service;
+    @Autowired
+    private PersonService personService;
     @GetMapping("/admin_panel")
     public String getAdminPanel(){
         return "admin/adminPanel";
@@ -32,5 +39,11 @@ public class AdminPanelController {
     public String bookRegister() {
         return "admin/bookRegister";
     }
-
+    @GetMapping("/all_people")
+    public ModelAndView getAllPeople() {
+        List<Person> list = personService.findAll();
+        ModelAndView modelAndView = new ModelAndView("admin/allPeople");
+        modelAndView.addObject("person", list);
+        return modelAndView;
+    }
 }
