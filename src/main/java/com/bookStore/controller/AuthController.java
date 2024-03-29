d0de65a0c9849737fe8d83a89d49b2182d466893
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-29 19:44:56 +0300

    feat: Добавил логику "Ограничение создание новой сущности" нельзя добавить двух пользователей с одним именем

--- src/main/java/com/bookStore/controller/AuthController.java
+++ src/main/java/com/bookStore/controller/AuthController.java
@@ -2,10 +2,10 @@ package com.bookStore.controller;
 
 
 import com.bookStore.entity.Person;
+import com.bookStore.service.PersonService;
 import com.bookStore.service.RegistrationServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/auth")
@@ -20,9 +22,12 @@ public class AuthController {
 
     private final RegistrationServices registrationServices;
 
+    private final PersonService personService;
+
     @Autowired
-    public AuthController(RegistrationServices registrationServices) {
+    public AuthController(RegistrationServices registrationServices, PersonService personService) {
         this.registrationServices = registrationServices;
+        this.personService = personService;
     }
 
     @GetMapping("/login")
@@ -34,11 +39,19 @@ public class AuthController {
         return "auth/registration";
     }
     @PostMapping("/registration")
-    public String registration(@ModelAttribute("person") @Valid Person person,
-                               BindingResult bindingResult){
+    public String register(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
         if (bindingResult.hasErrors()){
             return "auth/registration";
         }
+        // Проверяем, существует ли пользователь с таким же адресом электронной почты
+        Optional<Person> existingUsers = personService.getPersonByName(person.getUsername());
+        if (existingUsers.isPresent()) {
+            // Если пользователь существует, необходимо предпринять действия, например, вернуть ошибку или выбросить исключение
+            throw new IllegalArgumentException("Пользователь с таким именем уже существует");
+        }
+
+        // Если пользователя с таким адресом электронной почты не существует, сохраняем нового пользователя
         registrationServices.register(person);
         return "redirect:/auth/login";
     }
