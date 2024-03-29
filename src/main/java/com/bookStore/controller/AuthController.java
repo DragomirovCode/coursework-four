2e2b3c41042f82d16feca15cdc6643a0742b5cfc
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-29 12:04:29 +0300

    feat: Добавил проверку на поля при регистрации

--- src/main/java/com/bookStore/controller/AuthController.java
+++ src/main/java/com/bookStore/controller/AuthController.java
@@ -5,10 +5,14 @@ import com.bookStore.entity.Person;
 import com.bookStore.service.RegistrationServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
@@ -30,7 +34,11 @@ public class AuthController {
         return "auth/registration";
     }
     @PostMapping("/registration")
-    public String registration(@ModelAttribute("person") Person person){
+    public String registration(@ModelAttribute("person") @Valid Person person,
+                               BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "auth/registration";
+        }
         registrationServices.register(person);
         return "redirect:/auth/login";
     }
