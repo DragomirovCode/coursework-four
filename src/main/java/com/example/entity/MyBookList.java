fc3990b9cc0ac5ba39bdc5c3173e25c039a28ada
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-10 21:44:39 +0300

    chore: Изменил int на String

--- src/main/java/com/example/entity/MyBookList.java
+++ src/main/java/com/example/entity/MyBookList.java
@@ -14,9 +14,7 @@ import java.time.LocalDateTime;
 @Table(name="MyBooks")
 public class MyBookList {
 	@Id
-	@Column(name = "id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private String id;
 
 	@NotEmpty(message = "Название книги не должно быть пустым")
 	@Column(name = "name")
@@ -33,54 +31,19 @@ public class MyBookList {
 	@PositiveOrZero(message = "Количество книг должно быть неотрицательным")
 	@Column(name = "quantity")
 	private int quantity; // количество книг в наличии
+
 	private int currentUserId;
+
+	private LocalDateTime timestamp;
 	public MyBookList() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public MyBookList(int currentUserId, int id, String name, String author, int price, int quantity) {
-		super();
+	public MyBookList(String id, String name, String author, int price, int quantity) {
 		this.id = id;
 		this.name = name;
 		this.author = author;
 		this.price = price;
-		this.currentUserId = currentUserId;
-		this.quantity = quantity;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getCurrentUserId() {
-		return currentUserId;
-	}
-	public void setCurrentUserId(int currentUserId) {
-		this.currentUserId = currentUserId;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
 }
