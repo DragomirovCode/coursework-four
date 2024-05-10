9c34520c93a9ee7437d3e07e37661fb1c48ddd9b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-10 21:45:25 +0300

    feat: Обновил метод "getMyList"

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -23,6 +23,8 @@
 
 	import javax.validation.Valid;
 	import java.security.Principal;
+	import java.time.Instant;
+	import java.time.LocalDateTime;
 	import java.util.*;
 
 	@Controller
@@ -77,31 +79,41 @@
 
 		@Transactional
 		@RequestMapping(value = "/mylist/{id}", method = RequestMethod.POST)
-		public String getMyList(@PathVariable("id") int id, @RequestParam("quantity") int quantity) {
+		public String getMyList(@PathVariable("id") String id, @RequestParam("quantity") int quantity) {
 			// Получаем текущего пользователя
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			PersonDetails currentUserDetails = (PersonDetails) authentication.getPrincipal();
 			Person currentUser = currentUserDetails.getPerson();
 
-			Book book = service.getBookById(id);
+			Book book = service.getBookById(Integer.parseInt(id));
+
+			// Получаем текущее время в миллисекундах
+			long timestamp = Instant.now().toEpochMilli();
+
+			// Создаем уникальный идентификатор, используя ID пользователя и текущее время
+			String uniqueId = String.valueOf((currentUser.getId() + timestamp) / 2);
 
 			// Проверяем, что количество запрошенных книг меньше или равно доступному количеству
 			if (quantity > book.getQuantity() || quantity <= 0) {
 				// Обработка ошибки или вывод сообщения пользователю
 				// Например, можно выбросить исключение или отобразить сообщение пользователю и
 				// перенаправить его обратно на страницу с доступными книгами.
-				return "redirect:/available_books";
+				return "redirect:/available_products";
 			}
 
-			// Создаем экземпляр MyBookList для текущего пользователя
-			MyBookList myBook = new MyBookList(currentUser.getId(), book.getId(), book.getName(), book.getAuthor(), book.getPrice(), quantity);
+			// Создаем экземпляр MyProductList для текущего пользователя
+			// Используем этот уникальный идентификатор при создании нового экземпляра MyBookList
+			MyBookList myBook = new MyBookList(uniqueId, book.getName(), book.getAuthor(), book.getPrice(), quantity);
+			myBook.setCurrentUserId(currentUser.getId());
+
+			myBook.setTimestamp(LocalDateTime.now());
 			myBookService.saveMyBooks(myBook);
 
 			// Уменьшаем количество книг в наличии на выбранное количество
 			book.setQuantity(book.getQuantity() - quantity);
-			// Увеличиваем количество проданных книг на выбранное количество
+			// Увеличиваем количество проданных книг на количество продуктов в myBook
 			int soldQuantity = book.getSoldQuantity();
-			book.setSoldQuantity(soldQuantity + quantity);
+			book.setSoldQuantity(soldQuantity + myBook.getQuantity());
 			// Сохраняем изменения
 			service.save(book);
 
